cdfe28b2f85872447b8fe96f50e9a4bd029f8eda
zreed <zreed@live.com>
2016-08-26 11:40:12 -0400

    [SQOrbits] Rewrite using StarQuest

--- Bungeecord/SQOrbits/src/main/java/com/starquestminecraft/bungeecord/orbits/PlanetData.java
+++ Bungeecord/SQOrbits/src/main/java/com/starquestminecraft/bungeecord/orbits/PlanetData.java
@@ -1,29 +1,181 @@
 package com.starquestminecraft.bungeecord.orbits;
 
+import net.md_5.bungee.config.Configuration;
+
+/**
+ * A representation of the planet object stored in the config.
+ */
 public class PlanetData {
 
-    /**
-     * A representation of the planet object stored in the config.
-     */
-    double sunX;
-    double sunZ;
+    private static final double RADIANS_PER_DEGREE = Math.PI / 180;
 
-    String systemName;
-    String worldName;
-    int orbitY;
-    double speed;
-    double blocksFromSun;
+    private final String name;
 
-    int relativeRegionMinCornerX;
-    int relativeRegionMinCornerY;
-    int relativeRegionMinCornerZ;
+    private final double sun_x;
+    private final double sun_z;
 
-    int relativeRegionMaxCornerX;
-    int relativeRegionMaxCornerY;
-    int relativeRegionMaxCornerZ;
+    private final String system_name;
+    private final String world_name;
+    private final int orbit_y;
+    private final double speed;
+    private final double sun_distance;
+    private final int region_min_x_relative;
+    private final int region_min_y_relative;
+    private final int region_min_z_relative;
+    private final int region_max_x_relative;
+    private final int region_max_y_relative;
+    private final int region_max_z_relative;
+    private final String schematic_path;
 
-    String schematicPath;
-    //initially set to the value from the config, but is later changed if found in DB
-    double rotation;
+    private double rotation;
+    private int center_x;
+    private int center_z;
+
+    public PlanetData(final String name, final Configuration section) {
+
+        this.name = name;
+
+        this.sun_x = section.getDouble("SunCoords.x");
+        this.sun_z = section.getDouble("SunCoords.z");
+
+        this.system_name = section.getString("SystemName");
+        this.world_name = section.getString("WorldName");
+        this.orbit_y = section.getInt("OrbitCenterY");
+        this.speed = section.getDouble("Speed");
+        this.sun_distance = section.getDouble("BlocksFromSun");
+
+        this.region_min_x_relative = section.getInt("RelativeRegionMinCorner.x");
+        this.region_min_y_relative = section.getInt("RelativeRegionMinCorner.y");
+        this.region_min_z_relative = section.getInt("RelativeRegionMinCorner.z");
+
+        this.region_max_x_relative = section.getInt("RelativeRegionMaxCorner.x");
+        this.region_max_y_relative = section.getInt("RelativeRegionMaxCorner.y");
+        this.region_max_z_relative = section.getInt("RelativeRegionMaxCorner.z");
+
+        this.schematic_path = section.getString("SchematicPath");
+
+        this.rotation = section.getDouble("InitialRotation");
+
+        updateCenter();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSunX() {
+        return sun_x;
+    }
+
+    public double getSunZ() {
+        return sun_z;
+    }
+
+    public String getSystemName() {
+        return system_name;
+    }
+
+    public String getWorldName() {
+        return world_name;
+    }
+
+    public int getOrbitY() {
+        return orbit_y;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getBlocksFromSun() {
+        return sun_distance;
+    }
+
+    public int getRelativeRegionMinX() {
+        return region_min_x_relative;
+    }
+
+    public int getRelativeRegionMinY() {
+        return region_min_y_relative;
+    }
+
+    public int getRelativeRegionMinZ() {
+        return region_min_z_relative;
+    }
+
+    public int getRelativeRegionMaxX() {
+        return region_max_x_relative;
+    }
+
+    public int getRelativeRegionMaxY() {
+        return region_max_y_relative;
+    }
+
+    public int getRelativeRegionMaxZ() {
+        return region_max_z_relative;
+    }
+
+    public String getSchematicPath() {
+        return schematic_path;
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(final double rotation) {
+
+        this.rotation = rotation;
+
+        updateCenter();
+
+    }
+
+    public int getCenterX() {
+        return center_x;
+    }
+
+    public int getCenterY() {
+        return orbit_y;
+    }
+
+    public int getCenterZ() {
+        return center_z;
+    }
+
+    public int getRegionMinX() {
+        return getCenterX() + getRelativeRegionMinX();
+    }
+
+    public int getRegionMinY() {
+        return getCenterY() + getRelativeRegionMinY();
+    }
+
+    public int getRegionMinZ() {
+        return getCenterZ() + getRelativeRegionMinZ();
+    }
+
+    public int getRegionMaxX() {
+        return getCenterX() + getRelativeRegionMaxX();
+    }
+
+    public int getRegionMaxY() {
+        return getCenterY() + getRelativeRegionMaxY();
+    }
+
+    public int getRegionMaxZ() {
+        return getCenterZ() + getRelativeRegionMaxZ();
+    }
+
+    private void updateCenter() {
+
+        double sin = Math.sin(rotation * RADIANS_PER_DEGREE);
+        double cos = Math.cos(rotation * RADIANS_PER_DEGREE);
+
+        center_x = (int)Math.round(sun_distance * cos + sun_x);
+        center_z = (int)Math.round(sun_distance * sin + sun_z);
+
+    }
 
 }
--- Bungeecord/SQOrbits/src/main/java/com/starquestminecraft/bungeecord/orbits/SQOrbits.java
+++ Bungeecord/SQOrbits/src/main/java/com/starquestminecraft/bungeecord/orbits/SQOrbits.java
@@ -1,113 +1,118 @@
 package com.starquestminecraft.bungeecord.orbits;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-import com.google.common.io.ByteStreams;
-
-import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
-import net.md_5.bungee.config.YamlConfiguration;
 
-public class SQOrbits extends Plugin {
+import com.starquestminecraft.bungeecord.SQBungeePlugin;
+import com.starquestminecraft.bungeecord.StarQuest;
 
-    private static final double RADIANS_PER_DEGREE = Math.PI / 180;     //useful math constant
-    private static final String CHAT_PREFIX = "[SQOrbitsBungee]";
+public class SQOrbits extends SQBungeePlugin {
 
-    private File configFile;
-    private Configuration config;
-    private Map<String, PlanetData> planetData;
-    private Set<String> planetsToDelete;
-    private String hostName;
-    private String dbName;
-    private Integer port;
-    private String userName;
-    private String password;
-    private String tableName;
-    private String dbURL;
-    private Connection connection;
+    private static final String TABLE_NAME = "planet_orbits";
+    private static final String SQL_DELETE_ORBIT = "DELETE FROM `" + TABLE_NAME + "` WHERE `name`=?";
+    private static final String SQL_INSUPD_ORBITS = "INSERT INTO `" + TABLE_NAME + "` (`name`,`rotation`,`system_name`,`world_name`,`x`,`y`,`z`,`region_max_x`,`region_max_y`,`region_max_z`,`region_min_x`,`region_min_y`,`region_min_z`,`schematic_path`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE `rotation`=VALUES(`rotation`),`system_name`=VALUES(`system_name`),`world_name`=VALUES(`world_name`),`x`=VALUES(`x`),`y`=VALUES(`y`),`z`=VALUES(`z`),`region_max_x`=VALUES(`region_max_x`),`region_max_y`=VALUES(`region_max_y`),`region_max_z`=VALUES(`region_max_z`),`region_min_x`=VALUES(`region_min_x`),`region_min_y`=VALUES(`region_min_y`),`region_min_z`=VALUES(`region_min_z`),`schematic_path`=VALUES(`schematic_path`)";
+    private static final String SQL_SELECT_ROTATIONS = "SELECT `name`,`rotation` FROM `" + TABLE_NAME + "`";
+
+    private final Map<String, PlanetData> planets = new HashMap<>();
 
     @Override
-    public void onEnable() {
+    public void load() {
 
-        planetData = new HashMap<>();
-        planetsToDelete = new HashSet<>();
+        try(Connection con = StarQuest.getDatabaseConnection()) {
 
-        try {
-            setupFromConfig();
-        }
-        catch(BadConfigException ex) {
-            print("Errors setting up from config mean the plugin will stop running.");
-            return;
-        }
+            try(Statement s = con.createStatement()) {
 
-        try {
+                s.executeUpdate("CREATE TABLE IF NOT EXISTS `" + TABLE_NAME + "` ("
+                    + "`name` VARCHAR(32) NOT NULL,"
+                    + "`rotation` DOUBLE DEFAULT 0,"
+                    + "`system_name` VARCHAR(32) NOT NULL,"
+                    + "`world_name` VARCHAR(32) NOT NULL,"
+                    + "`x` INTEGER DEFAULT 0,"
+                    + "`y` INTEGER DEFAULT 0,"
+                    + "`z` INTEGER DEFAULT 0,"
+                    + "`region_max_x` INTEGER DEFAULT 0,"
+                    + "`region_max_y` INTEGER DEFAULT 0,"
+                    + "`region_max_z` INTEGER DEFAULT 0,"
+                    + "`region_min_x` INTEGER DEFAULT 0,"
+                    + "`region_min_y` INTEGER DEFAULT 0,"
+                    + "`region_min_z` INTEGER DEFAULT 0,"
+                    + "`schematic_path` VARCHAR(256) NOT NULL,"
+                    + "PRIMARY KEY(`name`))");
 
-            connectToDB();
+                logInfo("SQL table creation/checking was successful.");
 
-            Driver driver = (Driver)Class.forName("com.mysql.jdbc.Driver").newInstance();
-            DriverManager.registerDriver(driver);
-
-            String query = "CREATE TABLE IF NOT EXISTS `" + tableName + "` ("
-                + "`name` VARCHAR(32) NOT NULL,"
-                + "`rotation` DOUBLE DEFAULT 0,"
-                + "`system_name` VARCHAR(32) NOT NULL,"
-                + "`world_name` VARCHAR(32) NOT NULL,"
-                + "`x` INTEGER DEFAULT 0,"
-                + "`y` INTEGER DEFAULT 0,"
-                + "`z` INTEGER DEFAULT 0,"
-                + "`region_max_x` INTEGER DEFAULT 0,"
-                + "`region_max_y` INTEGER DEFAULT 0,"
-                + "`region_max_z` INTEGER DEFAULT 0,"
-                + "`region_min_x` INTEGER DEFAULT 0,"
-                + "`region_min_y` INTEGER DEFAULT 0,"
-                + "`region_min_z` INTEGER DEFAULT 0,"
-                + "`schematic_path` VARCHAR(256) NOT NULL,"
-                + "PRIMARY KEY(`name`))";
-
-            try(PreparedStatement ps = connection.prepareStatement(query)) {
-                ps.execute();
             }
 
-            print("SQL table creation/checking was successful.");
+        }
+        catch(SQLException ex) {
+            logSevere("Exception creating database table:", ex);
+        }
 
-            try(Statement s = connection.createStatement()) {
+    }
 
-                try(ResultSet rs = s.executeQuery("SELECT `name`, `rotation` FROM `" + tableName + "`")) {
+    @Override
+    public void enable() {
 
-                    planetsToDelete = new HashSet<>();
+        planets.clear();
 
-                    while(rs.next()) {   //iterates through all rows in set
+        loadFromConfig();
 
-                        String planetName = rs.getString("name");
-                        PlanetData data = planetData.get(planetName);
+        try(Connection con = StarQuest.getDatabaseConnection()) {
 
-                        //if planet not present in config (meaning it is old and should be deleted from the DB)
-                        //marks planet for deletion in DB
-                        if(data == null) {
-                            planetsToDelete.add(planetName);
-                        }
-                        //else (not a new planet as it is in DB but not one to be deleted as still in config)
-                        //changes the rotation value of the planet so it is moved
-                        else {
-                            data.rotation = rs.getDouble("rotation") + data.speed;
-                        }
+            Set<String> to_delete = loadCurrentRotations(con);
 
+            if(!to_delete.isEmpty()) {
+                deleteOldPlanets(con, to_delete);
+            }
+
+            updatePlanets(con);
+
+        }
+        catch(SQLException ex) {
+            logSevere("Exception accessing database:", ex);
+        }
+
+    }
+
+    private void addPlanetData(final PlanetData data) {
+        planets.put(data.getName().toLowerCase(), data);
+    }
+
+    public PlanetData getPlanetData(final String name) {
+        return planets.get(name.toLowerCase());
+    }
+
+    private Set<String> loadCurrentRotations(final Connection con) {
+
+        Set<String> to_delete = new HashSet<>();
+
+        try(Statement s = con.createStatement()) {
+
+            try(ResultSet rs = s.executeQuery(SQL_SELECT_ROTATIONS)) {
+
+                while(rs.next()) {
+
+                    String name = rs.getString("name");
+                    PlanetData data = getPlanetData(name);
+
+                    //if planet not present in config (meaning it is old and should be deleted from the DB)
+                    //marks planet for deletion in DB
+                    if(data == null) {
+                        to_delete.add(name);
+                    }
+                    //else (not a new planet as it is in DB but not one to be deleted as still in config)
+                    //changes the rotation value of the planet so it is moved
+                    else {
+                        data.setRotation(rs.getDouble("rotation") + data.getSpeed());
                     }
 
                 }
@@ -115,258 +120,82 @@ public class SQOrbits extends Plugin {
             }
 
         }
-        catch(ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException ex) {
-            print("An error connecting to or reading from the database mean the plugin will stop running. Stack trace: ");
-            ex.printStackTrace();
-            return;
-        }
-
-        deleteOldPlanets();
-        updateDB();
-
-        try {
-            connection.close();
-        }
         catch(SQLException ex) {
-            print("An error occurred whilst closing the SQL connection. Stack trace:");
-            ex.printStackTrace();
+            logSevere("Exception loading current planet rotations:", ex);
         }
+
+        return to_delete;
 
     }
 
-    private void updateDB() {
+    private void updatePlanets(final Connection con) {
 
-        String query = "INSERT INTO `" + tableName + "` ("
-            + "`name`,"
-            + "`rotation`,"
-            + "`system_name`,"
-            + "`world_name`,"
-            + "`x`,"
-            + "`y`,"
-            + "`z`,"
-            + "`region_max_x`,"
-            + "`region_max_y`,"
-            + "`region_max_z`,"
-            + "`region_min_x`,"
-            + "`region_min_y`,"
-            + "`region_min_z`,"
-            + "`schematic_path`"
-            + ") VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-            + "ON DUPLICATE KEY UPDATE "
-            + "`rotation`=VALUES(`rotation`),"
-            + "`system_name`=VALUES(`system_name`),"
-            + "`world_name`=VALUES(`world_name`),"
-            + "`x`=VALUES(`x`),"
-            + "`y`=VALUES(`y`),"
-            + "`z`=VALUES(`z`),"
-            + "`region_max_x`=VALUES(`region_max_x`),"
-            + "`region_max_y`=VALUES(`region_max_y`),"
-            + "`region_max_z`=VALUES(`region_max_z`),"
-            + "`region_min_x`=VALUES(`region_min_x`),"
-            + "`region_min_y`=VALUES(`region_min_y`),"
-            + "`region_min_z`=VALUES(`region_min_z`),"
-            + "`schematic_path`=VALUES(`schematic_path`)";
+        // I'm reasonably certain this can be batched
+        try(PreparedStatement ps = con.prepareStatement(SQL_INSUPD_ORBITS)) {
 
-        for(String planet_name : planetData.keySet()) {
+            for(PlanetData data : planets.values()) {
 
-            PlanetData data = planetData.get(planet_name);
-            int[] centreCoords = calculatePlanetCoords(planet_name, planetData.get(planet_name).rotation);
-            int centreX = centreCoords[0];
-            int centreY = centreCoords[1];
-            int centreZ = centreCoords[2];
-            int maxX = centreX + data.relativeRegionMaxCornerX;
-            int maxY = centreY + data.relativeRegionMaxCornerY;
-            int maxZ = centreZ + data.relativeRegionMaxCornerZ;
-            int minX = centreX + data.relativeRegionMinCornerX;
-            int minY = centreY + data.relativeRegionMinCornerY;
-            int minZ = centreZ + data.relativeRegionMinCornerZ;
+                ps.setString(1, data.getName());
+                ps.setDouble(2, data.getRotation());
+                ps.setString(3, data.getSystemName());
+                ps.setString(4, data.getWorldName());
+                ps.setInt(5, data.getCenterX());
+                ps.setInt(6, data.getCenterY());
+                ps.setInt(7, data.getCenterZ());
+                ps.setInt(8, data.getRegionMaxX());
+                ps.setInt(9, data.getRegionMaxY());
+                ps.setInt(10, data.getRegionMaxZ());
+                ps.setInt(11, data.getRegionMinX());
+                ps.setInt(12, data.getRegionMinY());
+                ps.setInt(13, data.getRegionMinZ());
+                ps.setString(14, data.getSchematicPath());
 
-            try(PreparedStatement ps = connection.prepareStatement(query)) {
-
-                ps.setString(1, planet_name);
-                ps.setDouble(2, data.rotation);
-                ps.setString(3, data.systemName);
-                ps.setString(4, data.worldName);
-                ps.setInt(5, centreX);
-                ps.setInt(6, centreY);
-                ps.setInt(7, centreZ);
-                ps.setInt(8, maxX);
-                ps.setInt(9, maxY);
-                ps.setInt(10, maxZ);
-                ps.setInt(11, minX);
-                ps.setInt(12, minY);
-                ps.setInt(13, minZ);
-                ps.setString(14, data.schematicPath);
-
-                ps.execute();
+                ps.addBatch();
 
             }
-            catch(SQLException ex) {
-                print("An error merging data for planet '" + planet_name + "' into the database. "
-                    + "As a result the planet will not be added. Stack trace: ");
-                ex.printStackTrace();
-            }
 
+            ps.executeBatch();
+
+        }
+        catch(SQLException ex) {
+            logSevere("Exception updating planet orbits:", ex);
         }
 
     }
 
     //NB: this method does not delete the planet structure+region in the world,
     //it only cleans it from the database so it won't move anymore
-    private void deleteOldPlanets() {
+    private void deleteOldPlanets(final Connection con, final Set<String> planets) {
 
-        String query = "DELETE FROM `" + tableName + "` WHERE `name`=?";
+        try(PreparedStatement ps = con.prepareStatement(SQL_DELETE_ORBIT)) {
 
-        for(String planetName : planetsToDelete) {
+            for(String name : planets) {
 
-            try(PreparedStatement ps = connection.prepareStatement(query)) {
-                
-                ps.setString(1, planetName);
-                
-                ps.execute();
+                ps.setString(1, name);
+
+                ps.addBatch();
 
             }
-            catch(SQLException ex) {
-                print("An SQLException occured when attempting to delete the planet '" + planetName + "' from the database. "
-                    + "As a result, the planet will not be deleted from the database.");
-            }
 
+            ps.executeBatch();
+
+        }
+        catch(SQLException ex) {
+            logSevere("Exception deleting planets from database:", ex);
         }
 
     }
 
-    //fetches all data but rotation from respective PlanetData object stored in map
-    private int[] calculatePlanetCoords(final String planetName, final double rotation) {
+    private void loadFromConfig() {
 
-        PlanetData data = planetData.get(planetName);
-        double sin = Math.sin(rotation * RADIANS_PER_DEGREE);
-        double cos = Math.cos(rotation * RADIANS_PER_DEGREE);
-        double rawCentreX = data.blocksFromSun * cos + data.sunX;
-        double rawCentreZ = data.blocksFromSun * sin + data.sunZ;
-        int x = (int)Math.round(rawCentreX);
-        int z = (int)Math.round(rawCentreZ);
-        int y = data.orbitY;
+        Configuration section_planets = getConfig().getSection("Planets");
 
-        return new int[] {x, y, z};
+        for(String planet_name : section_planets.getKeys()) {
 
-    }
+            Configuration section = section_planets.getSection(planet_name);
+            PlanetData data = new PlanetData(planet_name, section);
 
-    private void print(final String msg) {
-        getProxy().getLogger().info(CHAT_PREFIX + " " + msg);
-    }
-
-    private void setupFromConfig() throws BadConfigException {
-
-        saveDefaultConfig();
-
-        try {
-            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
-        }
-        catch(IOException ex) {
-            print("An error occurred loading the config file.");
-            ex.printStackTrace();
-            return;
-        }
-
-        hostName = getFromConfig("Host_Name");
-        dbName = getFromConfig("Database_Name");
-        port = getFromConfig("Port");
-        userName = getFromConfig("Username");
-        password = getFromConfig("Password");
-        tableName = getFromConfig("TableName");
-
-        LinkedHashMap<String, Object> planetSection = getFromConfig("Planets");
-        Collection<String> planetNames = planetSection.keySet();
-
-        for(String planet_name : planetNames) {
-
-            PlanetData planet = new PlanetData();
-
-            planet.sunX = getFromConfig("Planets." + planet_name + ".SunCoords.x");
-            planet.sunZ = getFromConfig("Planets." + planet_name + ".SunCoords.z");
-
-            planet.systemName = getFromConfig("Planets." + planet_name + ".SystemName");
-            planet.worldName = getFromConfig("Planets." + planet_name + ".WorldName");
-            planet.orbitY = getFromConfig("Planets." + planet_name + ".OrbitCenterY");
-            planet.speed = getFromConfig("Planets." + planet_name + ".Speed");
-            planet.blocksFromSun = getFromConfig("Planets." + planet_name + ".BlocksFromSun");
-
-            planet.relativeRegionMinCornerX = getFromConfig("Planets." + planet_name + ".RelativeRegionMinCorner.x");
-            planet.relativeRegionMinCornerY = getFromConfig("Planets." + planet_name + ".RelativeRegionMinCorner.y");
-            planet.relativeRegionMinCornerZ = getFromConfig("Planets." + planet_name + ".RelativeRegionMinCorner.z");
-
-            planet.relativeRegionMaxCornerX = getFromConfig("Planets." + planet_name + ".RelativeRegionMaxCorner.x");
-            planet.relativeRegionMaxCornerY = getFromConfig("Planets." + planet_name + ".RelativeRegionMaxCorner.y");
-            planet.relativeRegionMaxCornerZ = getFromConfig("Planets." + planet_name + ".RelativeRegionMaxCorner.z");
-
-            planet.schematicPath = getFromConfig("Planets." + planet_name + ".SchematicPath");
-
-            planet.rotation = getFromConfig("Planets." + planet_name + ".InitialRotation");
-
-            planetData.put(planet_name, planet);
-
-        }
-
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T extends Object> T getFromConfig(String configName) throws BadConfigException {
-
-        Object fromConfig = config.get(configName);
-
-        if(fromConfig == null) {
-            print("Error retrieving value from config field '" + configName + "': is null.");
-            throw new BadConfigException();
-        }
-
-        T tObject;
-        try {
-            tObject = (T)fromConfig;
-        }
-        catch(ClassCastException ex) {
-            print("Error retrieving value from config field '" + configName + "': invalid datatype.");
-            throw new BadConfigException();
-        }
-
-        return tObject;
-
-    }
-
-    private void connectToDB() throws SQLException {
-
-        dbURL = "jdbc:mysql://" + hostName + ":" + port + "/" + dbName;
-        if((userName.isEmpty()) && (password.isEmpty())) {
-            connection = DriverManager.getConnection(dbURL);
-        }
-        else {
-            connection = DriverManager.getConnection(dbURL, userName, password);
-        }
-
-    }
-
-    private void saveDefaultConfig() {
-
-        File dataFolder = getDataFolder();
-
-        if(!dataFolder.exists()) {
-            dataFolder.mkdir();
-        }
-
-        configFile = new File(dataFolder, "config.yml");
-
-        if(!configFile.exists()) {
-
-            try {
-
-                configFile.createNewFile();
-
-                ByteStreams.copy(getResourceAsStream("config.yml"), new FileOutputStream(configFile));
-
-            }
-            catch(IOException ex) {
-                print("An error occurred creating the default config file.");
-                ex.printStackTrace();
-            }
+            addPlanetData(data);
 
         }
 
