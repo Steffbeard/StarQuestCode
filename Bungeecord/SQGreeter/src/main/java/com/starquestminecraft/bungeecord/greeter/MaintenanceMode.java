e81882aa3e0c1d96788db6e762bacb39870e7208
zreed <zreed@live.com>
2016-08-25 21:46:20 -0400

    [SQGreeter] Use UUIDFetcher provided by StarQuest for username lookup

--- Bungeecord/SQGreeter/src/main/java/com/starquestminecraft/bungeecord/greeter/MaintenanceMode.java
+++ Bungeecord/SQGreeter/src/main/java/com/starquestminecraft/bungeecord/greeter/MaintenanceMode.java
@@ -3,14 +3,12 @@ package com.starquestminecraft.bungeecord.greeter;
 import java.util.ArrayList;
 import java.util.UUID;
 
-import com.mojang.api.profiles.HttpProfileRepository;
-import com.mojang.api.profiles.Profile;
+import com.starquestminecraft.bungeecord.util.UUIDFetcher;
 
 public class MaintenanceMode {
 	
 	private static boolean active = false;
 	private static ArrayList<UUID> allowedPlayers = new ArrayList<UUID>();
-	static HttpProfileRepository repo = new HttpProfileRepository("minecraft");
 
 	public static String message;
 	public static boolean isEnabled(){
@@ -41,22 +39,12 @@ public class MaintenanceMode {
 	}
 	
 	public static UUID uuidFromUsername(String username) {
-		Profile[] profiles = repo.findProfilesByNames(new String[] { username });
-		String s = profiles[0].getId();
-		String s2 = profiles[0].getName();
-		System.out.println(s);
+        UUIDFetcher.Profile profile = UUIDFetcher.getProfile(username);
+		UUID u = profile.getID();
+		String s2 = profile.getName();
+		System.out.println(u);
 		System.out.println(s2);
-		UUID u = uncanonicalizeUUID(s);
 		return u;
 	}
 
-	public static UUID uncanonicalizeUUID(String shortUUID) {
-		return UUID.fromString(shortUUIDToLong(shortUUID));
-	}
-
-	public static String shortUUIDToLong(String uuidString) {
-		if (uuidString.length() != 32)
-			throw new IllegalArgumentException("Wrong length");
-		return uuidString.substring(0, 8) + "-" + uuidString.substring(8, 12) + "-" + uuidString.substring(12, 16) + "-" + uuidString.substring(16, 20) + "-" + uuidString.substring(20, 32);
-	}
 }
